87bf778aac93230b0b7ef16e30d6f6738364d65d
Claudete Machado <54195430879@escola928.edu>
2024-03-18 17:11:43 -0300

    fds

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import Calculadora from './assets/components/Calculadora'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Calculadora />
+    </>
+  )
+}
+
+export default App
